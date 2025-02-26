5465a268aa6f15cef1bcd8dece760e817e67f93a
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-02-26 14:22:45 +0000

    agregado shacn al proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react'
-
+import path from 'path';
+console.log(path.resolve(__dirname, './src'))
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     watch: {
       usePolling: true,
