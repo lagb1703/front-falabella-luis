ae0a6ce03dc7e00afd94a4c3ec53d82d05c12a77
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-03-02 04:35:25 +0000

    merge jgalvis finish

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,35 +3,35 @@ import "./header.css";
 export default function Header() {
     return (
 
-    <header className="header">
-        
-        <SubHeader />
+        <header className="header">
 
-        <div className="main-header">
+            <SubHeader />
 
-            <div className="main-logo" >
-                <img src="https://images.falabella.com/v3/assets/blt088e6fffbba20f16/blt4c474b53ecc2a0ac/65e93b7882d68f0bd6d20cf9/falabella.com_green_icon_mobile.svg"
-                alt="Logo" />
+            <div className="main-header">
+
+                <div className="main-logo" >
+                    <img src="https://images.falabella.com/v3/assets/blt088e6fffbba20f16/blt4c474b53ecc2a0ac/65e93b7882d68f0bd6d20cf9/falabella.com_green_icon_mobile.svg"
+                        alt="Logo" />
+                </div>
+
+                <button className="menu-button">☰ Menú</button>
+
+                <div className="search-container">
+                    <input type="text" placeholder="Buscar en falabella.com" />
+                    <button>🔍</button>
+                </div>
+
+                <button className="login-button">Inicia sesión</button>
+
+                <button className="misCompras-button">Inicia sesión</button>
+
+                <button className="Heart-button">Inicia sesión</button>
+
+                <button className="Carrito">Inicia sesión</button>
+
             </div>
-            
-            <button className="menu-button">☰ Menú</button>
 
-            <div className="search-container">
-                <input type="text" placeholder="Buscar en falabella.com" />
-                <button>🔍</button>
-            </div>
-
-            <button className="login-button">Inicia sesión</button>
-
-            <button className="misCompras-button">Inicia sesión</button>
-
-            <button className="Heart-button">Inicia sesión</button>
-
-            <button className="Carrito">Inicia sesión</button>
-
-        </div>
-
-        </header> 
+        </header>
     );
 }
 
@@ -40,12 +40,12 @@ function SubHeader() {
         <div className="sub-header">
             <div className="falabella-bedcrums-logo">
                 <img src="https://images.falabella.com/v3/assets/blt088e6fffbba20f16/blt4c474b53ecc2a0ac/65e93b7882d68f0bd6d20cf9/falabella.com_green_icon_mobile.svg"
-                alt="Logo" />
+                    alt="Logo" />
             </div>
 
             <div className="homecenter-bedcrums-logo">
                 <img src="https://images.falabella.com/v3/assets/blt088e6fffbba20f16/bltccc5a7e27f9cab2f/6418aba238ab2c1b60a4bc29/logo-homecenter-our-stores.svg"
-                alt="Logo" />
+                    alt="Logo" />
             </div>
         </div>
     );
