4ff80885150ed40a458c907ce50496d3d05bc9ab
J-Galvis <galviscamilojuan@gmail.com>
2025-03-17 11:55:02 -0500

    merge luis & galvis

--- src/pages/examplePage/index.jsx
+++ src/pages/examplePage/index.jsx
@@ -1,7 +1,90 @@
+import { useGetInformation } from "./examplePage";
+import { useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import { Button, Box, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import Slider from "react-slick"; // npm install react-slick slick-carousel
 import "./examplePage.css";
 
 export default function ExamplePage() {
   return (
-    <h1>Pagina principal</h1>
+    <>
+      <img src="/home_images/anuncio1.png" alt="Imagen de primer anuncio" />
+      <Carousel />
+    </>
+  );
+}
+
+
+
+export function Carousel() {
+  
+  const [slider, setSlider] = useState(null);
+
+  const top = useBreakpointValue({ base: "90%", md: "50%" });
+  const side = useBreakpointValue({ base: "30%", md: "10px" });
+
+  const cards = [
+    "/home_images/anuncio2.png",
+    "/home_images/anuncio3.png",
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,  
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1, 
+    arrows: false,
+    autoplay: true,  
+    autoplaySpeed: 3000
+  };
+
+  return (
+    <Box position={"relative"} height={"345px"} width={"full"} overflow={"hidden"}>
+      {/* Left Icon */}
+      <IconButton
+        aria-label="left-arrow"
+        colorScheme="messenger"
+        borderRadius="full"
+        position="absolute"
+        left={side}
+        top={top}
+        transform={"translate(0%, -50%)"}
+        zIndex={2}
+        onClick={() => slider?.slickPrev()}
+      >
+        <IoIosArrowBack />
+      </IconButton>
+      {/* Right Icon */}
+      <IconButton
+        aria-label="right-arrow"
+        colorScheme="messenger"
+        borderRadius="full"
+        position="absolute"
+        right={side}
+        top={top}
+        transform={"translate(0%, -50%)"}
+        zIndex={2}
+        onClick={() => slider?.slickNext()}
+      >
+        <IoIosArrowForward />
+      </IconButton>
+      {/* Slider */}
+      <Slider {...settings} ref={setSlider}>
+        {cards.map((url, index) => (
+          <Box
+            key={index}
+            height={"345px"}
+            position="relative"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            backgroundSize="cover"
+            backgroundImage={`url(${url})`}
+          />
+        ))}
+      </Slider>
+    </Box>
   );
 }
