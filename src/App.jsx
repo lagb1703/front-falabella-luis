d161a938bfeeccc8d9687fa127bed7a6af96c075
Isa Hevia <79266318+iHevia@users.noreply.github.com>
2025-03-25 05:08:41 +0000

    products

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import theme from "./theme";
 import FooterComponent from './components/footer';
 import Header from "./components/header";
 import Menu from './components/menu'
+import products from './components/products'
 import ShoppingCartContext from './gobal/shoppingCart/shoppingCart.global';
 import UserContext from "./gobal/user/user.global"
 import './App.css'
