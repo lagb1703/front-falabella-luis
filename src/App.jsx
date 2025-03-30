a4429e1de427969619717194ea843774504ff325
J-Galvis <galviscamilojuan@gmail.com>
2025-03-30 01:47:44 -0500

    merge with the hope of god

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { projectRoutes } from "./pages";
 import theme from "./theme";
 import FooterComponent from './components/footer';
 import Header from "./components/header";
+import Menu from './components/menu'
+import products from './pages/productCategories'
 import ShoppingCartContext from './gobal/shoppingCart/shoppingCart.global';
 import UserContext from "./gobal/user/user.global"
 import './App.css'
