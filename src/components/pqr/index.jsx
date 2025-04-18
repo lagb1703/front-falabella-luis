0495cab3f8ddc2931247b97892b0cba15bd43d99
Isa Hevia <79266318+iHevia@users.noreply.github.com>
2025-04-18 17:45:46 +0000

    pqr

--- src/components/pqr/index.jsx
+++ src/components/pqr/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { PiGreaterThanLight } from "react-icons/pi";
+import { FaWhatsapp,FaInstagram, FaFacebookF } from "react-icons/fa";
 import './pqr.css';
 
 const CentroAyuda = () => {
@@ -115,6 +116,66 @@ const CentroAyuda = () => {
             )}
           </div>
         ))}
+      </div>
+      {/* Sección de Contáctanos */}
+      <div className="seccion-contactanos">
+        <h2 className="titulo-contactanos">Contáctanos</h2>
+        
+        <div className="contenedor-principal-contactos">
+          <div className="contenedor-contactos">
+            <div className="contacto-item">
+              <FaWhatsapp className="icono-contacto" />
+              <h3>Servicio al cliente</h3>
+              <p>Peticiones, quejas y reclamos</p>
+              <p className="telefono">+57 601 587 8002</p>
+              <p>Lunes a sábado:</p>
+              <p>09:00a.m. a 07:00p.m.</p>
+              <button className="boton-llamar">Llamar</button>
+            </div>
+            
+            <div className="contacto-item">
+              <FaWhatsapp className="icono-contacto" />
+              <h3>Whatsapp</h3>
+              <p>Peticiones, quejas y reclamos</p>
+              <p className="telefono">+57 1 897 7776</p>
+              <p>Lunes a sábado:</p>
+              <p>09:00a.m. a 07:00p.m.</p>
+              <button className="boton-llamar">Contactar</button>
+            </div>
+            
+            <div className="contacto-item">
+              <FaWhatsapp className="icono-contacto" />
+              <h3>Comprar por WhatsApp</h3>
+              <p className="telefono">+57 301 638 4422</p>
+              <p>Lunes a sábado:</p>
+              <p>08:00a.m. a 08:30p.m.</p>
+              <p>Domingos y festivos</p>
+              <p>08:00a.m. a 07:30p.m.</p>
+              <button className="boton-llamar">Comprar</button>
+            </div>
+            
+            <div className="contacto-item">
+              <FaWhatsapp className="icono-contacto" />
+              <h3>Club de Novios, Bebé o Deco</h3>
+              <p className="telefono">+57 301 633 4422</p>
+              <p>Lunes a domingos</p>
+              <p>10:00a.m. a 05:00p.m.</p>
+              <button className="boton-llamar">Llamar</button>
+            </div>
+          </div>
+        </div>
+        
+        <div className="footer-redes">
+          <h3 className="titulo-redes">Síguenos en nuestras redes</h3>
+          <div className="contenedor-iconos-redes">
+            <a href="#" className="boton-redes instagram">
+              <FaInstagram className="icono-red" />
+            </a>
+            <a href="#" className="boton-redes facebook">
+              <FaFacebookF className="icono-red" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
